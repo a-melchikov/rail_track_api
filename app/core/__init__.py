e529b37340980bc7a6d7f5fdc1c77d68cc2accae
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-09-26 14:29:05 +0400

    Настроен alembic, миграции перенесены в app.

---
+++ app/core/__init__.py
@@ -0,0 +1,6 @@
+__all__ = [
+    "settings",
+    "get_db_url",
+]
+
+from .config import settings, get_db_url
--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,6 +1,8 @@
 __all__ = [
     "db_helper",
     "DatabaseHelper",
+    "Base",
 ]
 
 from .session import db_helper, DatabaseHelper
+from .base import Base
