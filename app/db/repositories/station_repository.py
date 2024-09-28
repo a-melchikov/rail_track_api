9b4271e969f12b8b158e9383f48e151cc27ab30c
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-09-28 15:14:58 +0400

    Добавлен файл station_repository.

--- app/db/repositories/station_repository.py
+++ app/db/repositories/station_repository.py
@@ -1,13 +1,58 @@
+import asyncio
+
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from db import Station
+
+from db import Station, db_helper
+from schemas.station import StationCreate, StationUpdate
 
 
 class StationRepository:
-    def __init__(
-        self,
-        session: AsyncSession,
-    ):
+    def __init__(self, session: AsyncSession):
         self.session = session
 
-    def create_station() -> Station:
+    async def create_station(
+        self,
+        station_in: StationCreate,
+    ) -> Station:
+        station = Station(**station_in.model_dump())
+        self.session.add(station)
+        await self.session.commit()
+        return station
+
+    async def get_station_by_id(
+        self,
+        station_id: int,
+    ) -> Station | None:
+        return await self.session.get(Station, station_id)
+
+    async def get_all_stations(
+        self,
+    ) -> list[Station]:
+        stmt = select(Station).order_by(Station.id)
+        stations = await self.session.scalars(stmt)
+        return list(stations)
+
+    async def update_station(
+        self, station: Station, station_update: StationUpdate
+    ) -> Station:
+        for name, value in station_update.model_dump(exclude_unset=True).items():
+            setattr(station, name, value)
+        await self.session.commit()
+        return station
+
+    async def delete_station(
+        self,
+        station: Station,
+    ) -> None:
+        await self.session.delete(station)
+        await self.session.commit()
+
+
+async def main():
+    async with db_helper.session_factory() as session:
         pass
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
