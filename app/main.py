0c7775178e38daaafabd4490f685d6130d89c098
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-09-28 18:01:51 +0400

    Использование ORJson.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from contextlib import asynccontextmanager
+import uvicorn
+
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
 from api_v1 import router as router_v1
 from core import settings
@@ -13,11 +16,10 @@ async def lifespan(app: FastAPI):
 
 
 main_app = FastAPI(
+    default_response_class=ORJSONResponse,
     lifespan=lifespan,
 )
 main_app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 
 if __name__ == "__main__":
-    import uvicorn
-
     uvicorn.run("main:main_app", reload=True)
