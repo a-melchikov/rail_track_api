52caf0960be477ff376c74754be0f23eb457cf90
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-09-30 10:51:19 +0400

    Создана миграция для таблицы с типами поездов.

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -4,9 +4,11 @@ __all__ = [
     "Base",
     "Address",
     "Station",
+    "TrainType",
 ]
 
 from .session import db_helper, DatabaseHelper
 from .base import Base
 from .models.address import Address
 from .models.station import Station
+from .models.train_type import TrainType
