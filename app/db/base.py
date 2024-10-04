7056f084fe4bb3458b549ce74a912977512170e9
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-10-04 12:57:39 +0400

    Класс Base разделен на Mixin для id и сам класс Base.

--- app/db/base.py
+++ app/db/base.py
@@ -1,8 +1,6 @@
 from sqlalchemy.orm import (
     DeclarativeBase,
     declared_attr,
-    Mapped,
-    mapped_column,
 )
 
 
@@ -12,5 +10,3 @@ class Base(DeclarativeBase):
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f"{cls.__name__.lower()}s"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
